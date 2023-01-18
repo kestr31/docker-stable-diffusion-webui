32d8c9c956f909c23cb4f248147dbe0fd5fa8745
kestrel <samsani@naver.com>
2023-01-18 23:50:46 +0900

    Removed since xformers is now installed inside an app container

--- xformers/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-cd /root/xformers
-pip install -r requirements.txt ninja
-pip install --upgrade --extra-index-url https://download.pytorch.org/whl/cu113 torch torchvision torchaudio
-python3 setup.py build
-python3 setup.py bdist_wheel --universal
